f5e4cd03fffe4d82bbf6ab1f6bbab6de84225dcb
Pongchai Charoenrungrodkun <53739513+pongchai-charoenrungrodkun@users.noreply.github.com>
2024-02-14 08:40:42 +0700

    Update GreetingControllerTests.java

--- complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -37,16 +37,16 @@ public class GreetingControllerTests {
 	@Test
 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-		this.mockMvc.perform(get("/helloworld")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World! KU ***"));
+		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").value("Hello, World!"));
 	}
 
 	@Test
 	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
-		this.mockMvc.perform(get("/helloworld").param("name", "Spring Community"))
+		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
 				.andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, Spring Community! KU ***"));
+				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
 	}
 
 }
