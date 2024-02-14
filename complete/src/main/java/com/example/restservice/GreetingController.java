792a3dbf8dc92504871a5c8ce023551a8e3e663b
Pongchai Charoenrungrodkun <53739513+pongchai-charoenrungrodkun@users.noreply.github.com>
2024-02-14 08:39:54 +0700

    Update GreetingController.java

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s! KU ***";
+	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/helloworld")
+	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
